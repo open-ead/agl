d09c483b4a5a823b19ffdde53d8995a16bf3948b
MonsterDruide1 <5958456@gmail.com>
2021-12-17 15:05:52 +0100

    Add all headers from OdysseyReversed

---
+++ include/agl/aglDisplayList.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include "agl/gpu.h"
+
+namespace sead {
+class Heap;
+}
+
+namespace agl {
+class DisplayList {
+public:
+    DisplayList();
+    virtual ~DisplayList();
+
+    void setControlMemory(void*, s32);
+    void clear();
+    void setBuffer(GPUMemAddr<u8>, u64);
+    void setValidSize(u64 size);
+    void copyTo(DisplayList*) const;
+    void beginDisplayList();
+    void endDisplayList();
+    bool beginDisplayListBuffer(GPUMemAddr<u8>, u64, bool);
+    void endDisplayListBuffer(sead::Heap*);
+    void adjustValueSize();
+    void invalidateCPUCache() const;
+    void dump() const;
+    bool suspend(void**);
+    void resume(void*, u64);
+    u64 calcRemainingSize();
+
+private:
+    u64 _8;
+    u64 _10;
+    u64 _18;
+    u32 mUsedSize;
+    u32 _24;
+    u32 mSize;
+    u32 _2c;
+    u64 _30;
+    u64 _38;
+    u8 _40[0x248 - 0x40];  // todo; what is here?
+    u32 _248;              // init'd to 0x200
+    u32 _24c;
+    u64 _250;
+    const char* mDisplayName;
+};
+}  // namespace agl
---
+++ include/agl/shtxt/aglLexer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+namespace sead {
+class Heap;
+}
+
+namespace agl::shtxt {
+class Clause;
+
+class Lexer {
+public:
+    Lexer();
+    ~Lexer();
+
+    void initialize(sead::Heap*, char const*, Clause*);
+    void setupCurrentRange(u64 range);
+    u32 findNumberBlock() const;
+    Clause* createClause(u32) const;
+    bool execute(bool);
+
+private:
+    sead::Heap* _0;
+    Clause* _8;
+    char* mRefName;  // _10
+    u64 _18;
+    u64 _20;
+    u64 _28;
+};
+}  // namespace agl::shtxt
