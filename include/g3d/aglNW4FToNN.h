ad058ca883e817035453958f9dfd9e1234c918a0
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-04-04 17:21:03 +0200

    add:ResFile::Setup function

--- include/g3d/aglNW4FToNN.h
+++ include/g3d/aglNW4FToNN.h
@@ -2,14 +2,13 @@
 
 namespace nn::g3d {
 class ResFile;
-}
+}  // namespace nn::g3d
 
 namespace agl::g3d {
-
 class ResFile {
 public:
+    static void Setup(nn::g3d::ResFile*);
     static void BindTexture(nn::g3d::ResFile*, nn::g3d::ResFile*);
     static void Cleanup(nn::g3d::ResFile*);
 };
-
 }  // namespace agl::g3d
