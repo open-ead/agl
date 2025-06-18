62579f8f33a1cdb5ec981c49fda37ec754757888
ud2 <sjx233@qq.com>
2025-06-19 02:51:29 +0800

    g3d: Add `ResFile::GetTextureIndex` (#19)

--- include/g3d/aglNW4FToNN.h
+++ include/g3d/aglNW4FToNN.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <basis/seadTypes.h>
+
 namespace nn::g3d {
 class ResFile;
-}  // namespace nn::g3d
+}
 
 namespace agl::g3d {
+
 class ResFile {
 public:
     static void Setup(nn::g3d::ResFile*);
-    static void BindTexture(nn::g3d::ResFile*, nn::g3d::ResFile*);
     static void Cleanup(nn::g3d::ResFile*);
+    static s32 GetTextureIndex(const nn::g3d::ResFile*, const char*);
+    static bool BindTexture(nn::g3d::ResFile*, const nn::g3d::ResFile*);
 };
+
 }  // namespace agl::g3d
